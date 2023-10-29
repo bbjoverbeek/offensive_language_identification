122efaf71812662e7e94508d1316d0c49906b01e
Björn Overbeek <bbjoverbeek@gmail.com>
2023-10-29 11:07:33 +0100

    Add util.py

---
+++ src/util.py
@@ -0,0 +1,18 @@
+"""Util file for training models to identify offensive language"""
+
+
+def load_data(file_path: str) -> tuple[list[str], list[str]]:
+    """Loads the data from the given file path and returns the tweets and labels"""
+
+    tweets = []
+    labels = []
+
+    with open(file_path, 'r', encoding='utf-8') as inp:
+        for line in inp:
+            line = line.strip()
+            if line:
+                tweet, label = line.split('\t')
+                tweets.append(tweet)
+                labels.append(label)
+
+    return tweets, labels
