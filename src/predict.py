1989010c53b8bad6cf0f5c993c4b16bffbd5d042
Björn from Hábrók <b.b.j.overbeek@student.rug.nl>
2023-11-06 20:06:05 +0100

    merge files

--- src/predict.py
+++ src/predict.py
@@ -1,46 +1,4 @@
 import argparse
-<<<<<<< HEAD
-
-def parse_args() -> argparse.Namespace:
-    """Parse command line arguments"""
-
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument(
-        '-m',
-        '--model',
-        default='./model/',
-        help='The model to evaluate (default ./model/)'
-    )
-
-    parser.add_argument(
-        '-d',
-        'data-folder',
-        default='./data/',
-        help='folder that contains the data files (default ./data/)'
-    )
-    
-    parser.add_argument(
-        '-p'
-        '--predictions-file',
-        default='./predictions.txt'
-        help='file to save predictions to (defualt ./predictions.txt)'
-        
-    )
-
-    python3 $EXPERIMENT_FOLDER"predict.py" \
-            --model $EXPERIMENT_FOLDER"model/" \
-            --test-data $DATA_FOLDER"dev.tsv" \
-            --predictions-outp $EXPERIMENT_FOLDER"dev_predictions.txt"
-def predict(model_path: str) -> list[int]:
-    pass
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
-=======
 import os.path
 import pickle
 from enum import Enum
@@ -144,4 +102,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
->>>>>>> 97e843061b38072f69c0c9f27cfd0fb9a6d3ef75
